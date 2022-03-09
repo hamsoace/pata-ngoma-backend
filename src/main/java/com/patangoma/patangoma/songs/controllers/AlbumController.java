3d61d69f5a25371eae603ef017839e8fa01de9f9
hamsoace <hamsoace@gmail.com>
2022-03-09 18:59:47 +0300

    Album Controller

--- src/main/java/com/patangoma/patangoma/songs/controllers/AlbumController.java
+++ src/main/java/com/patangoma/patangoma/songs/controllers/AlbumController.java
@@ -1,10 +1,11 @@
 package com.patangoma.patangoma.songs.controllers;
 
+import com.patangoma.patangoma.songs.models.Album;
 import com.patangoma.patangoma.songs.services.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/songs/album")
@@ -13,4 +14,37 @@ public class AlbumController {
     private final AlbumService albumService;
 
     @Autowired
+    public AlbumController(AlbumService albumService){
+        this.albumService = albumService;
+    }
+
+    @GetMapping
+    public List<Album> getAllAlbum(){
+        return albumService.getAllAlbum();
+    }
+
+    @GetMapping("/{id}")
+    public Album getAlbumByAlbumId(@PathVariable("id") Long albumId) {
+        return albumService.getAlbumByAlbumId(albumId);
+    }
+
+    @PostMapping
+    public void createAlbum(Album album) {
+        albumService.createAlbum(album);
+    }
+
+    @DeleteMapping
+    public void deleteAlbum(Long id) {
+        albumService.deleteAlbum(id);
+    }
+
+    @PutMapping
+    public void updateAlbum(@RequestBody Album album) {
+        albumService.updateAlbum(album);
+    }
+
+    @GetMapping("/get")
+    public List<Album> getAllAlbumName(@RequestParam("name") String name) {
+        return albumService.getAllAlbumName(name);
+    }
 }
