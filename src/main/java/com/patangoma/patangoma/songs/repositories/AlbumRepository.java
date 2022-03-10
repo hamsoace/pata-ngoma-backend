c9052d51e5f49b0c128c1fadd608669f35e4d896
hamsoace <hamsoace@gmail.com>
2022-03-10 16:36:56 +0300

    album repository

--- src/main/java/com/patangoma/patangoma/songs/repositories/AlbumRepository.java
+++ src/main/java/com/patangoma/patangoma/songs/repositories/AlbumRepository.java
@@ -1,4 +1,63 @@
 package com.patangoma.patangoma.songs.repositories;
 
+import com.patangoma.patangoma.songs.models.Album;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
 public class AlbumRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void deleteById(Long id){
+        entityManager.remove(entityManager.find(Album.class, id));
+    }
+
+    @Transactional
+    public void save(Album album) {
+        entityManager.persist(album);
+    }
+
+    @Transactional
+    public List<Album> findAll() {
+        return entityManager.createQuery("select al from Album al", Album.class).getResultList();
+    }
+
+    @Transactional
+    public Album findBySongId(Long songId) {
+        return entityManager.find(Album.class, songId);
+    }
+
+    public void addAlbum(Long id, Album album) {
+        album.setSongId(id);
+        entityManager.persist(album);
+    }
+
+    public void updateAlbum(Long albumId, Album album) {
+        Album album1 = entityManager.find(Album.class, albumId);
+        if (album1 != null && album1.getAlbumName() != null) {
+            album1.setAlbumName(album.getAlbumName());
+        }
+        if (album.getAlbumDescription() != null) {
+            album1.setAlbumDescription(album.getAlbumDescription());
+        }
+        entityManager.persist(album1);
+    }
+
+    public void deleteAlbum(Long albumId) {
+        Album productCategory = entityManager.find(Album.class, albumId);
+        if (productCategory != null) {
+            entityManager.remove(productCategory);
+        }
+    }
+
+    public Album getAlbum(Long albumId) {
+        return entityManager.find(Album.class, albumId);
+    }
 }
